c4a74d1718ea567cbf06220041143fb08bcdedc5
Michael Ahern <michael.ahern@gmail.com>
2025-06-07 18:15:37 +0000

    cwd

--- src/mac.ts
+++ src/mac.ts
@@ -58,8 +58,6 @@ export class MacPlatform extends PlatformCommands {
             `    <string>${storagePath}/matterbridge.log</string>`,
             '    <key>StandardErrorPath</key>',
             `    <string>${storagePath}/matterbridge.log</string>`,
-            '    <key>WorkingDirectory</key>',
-            `    <string>${pluginPath}</string>`,
             '</dict>',
             '</plist>'
         ].filter(x => x).join('\n');
