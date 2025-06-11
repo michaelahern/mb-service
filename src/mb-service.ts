77e3e10aa56fed11ff569edd28aa43f39e0bd376
Michael Ahern <michael.ahern@gmail.com>
2025-06-11 02:09:50 +0000

    v0.2.0

--- src/mb-service.ts
+++ src/mb-service.ts
@@ -1,31 +1,44 @@
 #!/usr/bin/env node
 
+import { readFileSync } from 'node:fs';
 import { platform } from 'node:os';
 import { parseArgs } from 'node:util';
 import { MacPlatform } from './mac.js';
 import { PlatformCommands } from './platform.js';
 
 async function main() {
-    const { positionals } = parseArgs({
+    const args = parseArgs({
         args: process.argv.slice(2),
-        options: { },
+        options: {
+            help: { type: 'boolean', short: 'h' },
+            version: { type: 'boolean', short: 'v' } },
+        strict: false,
         allowPositionals: true
     });
+
+    if (args.values.help) {
+        help();
+        process.exit(0);
+    }
+
+    if (args.values.version) {
+        const packageJson = readFileSync('./package.json', 'utf8');
+        const packageInfo = JSON.parse(packageJson);
+        console.log(packageInfo.version);
+        process.exit(0);
+    }
 
     let platformCommands: PlatformCommands;
     switch (platform()) {
         case 'darwin':
             platformCommands = new MacPlatform();
             break;
-        // case 'linux':
-        //     platformCommands = new LinuxPlatform();
-        //     break;
         default:
             console.log('Platform not supported:', platform());
             process.exit(2);
     }
 
-    const command = positionals[0];
+    const command = args.positionals[0];
     switch (command) {
         case 'install':
             platformCommands.install();
@@ -67,7 +80,8 @@ async function main() {
 }
 
 function help() {
-    console.log('Usage: mb-service <command>');
+    console.log('Usage: mb-service <command> [options]');
+    console.log('');
     console.log('Commands:');
     console.log('  install       Install the Matterbridge service');
     console.log('  uninstall     Uninstall the Matterbridge service');
@@ -76,6 +90,11 @@ function help() {
     console.log('  restart       Restart the Matterbridge service');
     console.log('  pid           Get the process id of the Matterbridge service');
     console.log('  tail          Tail the Matterbridge log file');
+    console.log('');
+    console.log('Options:');
+    console.log('  -h, --help');
+    console.log('  -v, --version');
+    console.log('');
 }
 
 main().catch(err => console.error(err));
