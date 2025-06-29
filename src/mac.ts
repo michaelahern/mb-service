1bf54ab98ffb9f2009e49d83963e43b71553c9a0
Michael Ahern <michael.ahern@gmail.com>
2025-06-29 21:41:24 +0000

    Mac -nosudo

--- src/mac.ts
+++ src/mac.ts
@@ -34,6 +34,11 @@ export class MacPlatform extends PlatformCommands {
             }
         }
 
+        // Add -nosudo given not required with above chown
+        if (!args.includes('-nosudo')) {
+            args.push('-nosudo');
+        }
+
         // Create the launchd plist file
         const plistFileContents = [
             '<?xml version="1.0" encoding="UTF-8"?>',
